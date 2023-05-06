6b3a10395deb45bb4875d11e3714e6c21a8416d4
Svetlana Alekseeva <lanaleks90gmail.com>
2023-05-07 00:23:34 +0400

    Initial commit

---
+++ src/test/java/ru/netology/page/object/test/DeadlineSQLTest.java
@@ -0,0 +1,26 @@
+package ru.netology.page.object.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.page.object.data.DataHelper;
+import ru.netology.page.object.data.SQLHelper;
+import ru.netology.page.object.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.page.object.data.SQLHelper.cleanDatabase;
+
+public class DeadlineSQLTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+    @Test
+    void successfulLoginToYourPersonalAccount(){
+        open("http://localhost:9999");
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = LoginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+}
